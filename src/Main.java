634314974e7a60e1bc96968aa514df5e2159989b
hunbk <shyun823@gmail.com>
2024-03-25 20:13:41 +0900

    Squashed commit of the following:
    commit 889a1ba3f69a5b5281fa7c5309b1b0f5cbc07ac3
    Author: hunbk <shyun823@gmail.com>
    Date:   Mon Mar 25 20:02:21 2024 +0900
    
        commit 3
    
    commit a73edd3c46748e0947e0b057726ea9642f93fc65
    Author: hunbk <shyun823@gmail.com>
    Date:   Mon Mar 25 20:02:08 2024 +0900
    
        commit 2
    
    commit 49ff5fb7809e97286b907b53523d763a2185ad67
    Author: hunbk <shyun823@gmail.com>
    Date:   Mon Mar 25 20:01:43 2024 +0900
    
        commit 1

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("hi");
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
     }
 }
